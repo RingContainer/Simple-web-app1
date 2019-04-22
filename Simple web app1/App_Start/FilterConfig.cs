f6d608e8cb91193ebd4b0b51f8317a54a2edcc05
stephen.russell <stephen.russell@ringcontainer.com>
2019-04-22 11:00:42 -0500

    Add project files.

---
+++ Simple web app1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_web_app1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Simple web app1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Simple_web_app1.MvcApplication" Language="C#" %>
